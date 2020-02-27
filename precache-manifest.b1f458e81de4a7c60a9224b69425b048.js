be5895408643576c37d46b47f8a0f1ce2bbcff11
meriem23 <esra.msallem@gmail.com>
2020-02-27 15:23:31 +0100

    Updates

---
+++ precache-manifest.b1f458e81de4a7c60a9224b69425b048.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81160b77643acdf49ec107778a3b6c5b",
+    "url": "/React-NavMenu/index.html"
+  },
+  {
+    "revision": "c5418b5717dc6304c763",
+    "url": "/React-NavMenu/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "0c48d638c676c489d2c5",
+    "url": "/React-NavMenu/static/css/main.63fc5db5.chunk.css"
+  },
+  {
+    "revision": "c5418b5717dc6304c763",
+    "url": "/React-NavMenu/static/js/2.531787a5.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/React-NavMenu/static/js/2.531787a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c48d638c676c489d2c5",
+    "url": "/React-NavMenu/static/js/main.574d2f9e.chunk.js"
+  },
+  {
+    "revision": "2dd444a6642c9fdad9b5",
+    "url": "/React-NavMenu/static/js/runtime-main.e02f2912.js"
+  }
+]);
